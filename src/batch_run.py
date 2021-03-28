8d4b952100d92339b69887a7c622e0643e51356a
jvfe <jvfecav@gmail.com>
2021-03-28 14:39:58 -0300

    refactor: Separate batch run and run

---
+++ src/batch_run.py
@@ -0,0 +1,28 @@
+from model.model import CovidModel
+from mesa.batchrunner import BatchRunner
+
+modelo_normal = CovidModel(
+    1500, 3, insert_variant=False, width=60, height=60, seed=1024
+)
+modelo_variante = CovidModel(
+    1500, 3, insert_variant=True, width=60, height=60, seed=1024
+)
+
+for _ in range(2000):
+    modelo_normal.step()
+
+
+data = modelo_normal.datacollector.get_model_vars_dataframe()
+
+print(f"Rodando modelo normal por 2000 iterações:\n{data}")
+
+data.to_csv("../data/modelo_comum_dados.csv")
+
+for _ in range(2000):
+    modelo_variante.step()
+
+data_variante = modelo_variante.datacollector.get_model_vars_dataframe()
+
+print(f"Rodando modelo variante por 2000 iterações:\n{data_variante}")
+
+data_variante.to_csv("../data/modelo_variante_dados.csv")
--- src/run.py
+++ src/run.py
@@ -1,32 +1,3 @@
-from model.model import CovidModel
-from mesa.batchrunner import BatchRunner
+from server import server
 
-# from server import server
-
-modelo_normal = CovidModel(
-    1500, 3, insert_variant=False, width=60, height=60, seed=1024
-)
-modelo_variante = CovidModel(
-    1500, 3, insert_variant=True, width=60, height=60, seed=1024
-)
-
-for _ in range(2000):
-    modelo_normal.step()
-
-
-data = modelo_normal.datacollector.get_model_vars_dataframe()
-
-print(f"Rodando modelo normal por 2000 iterações:\n{data}")
-
-data.to_csv("../data/modelo_comum_dados.csv")
-
-for _ in range(2000):
-    modelo_variante.step()
-
-data_variante = modelo_variante.datacollector.get_model_vars_dataframe()
-
-print(f"Rodando modelo variante por 2000 iterações:\n{data_variante}")
-
-data_variante.to_csv("../data/modelo_variante_dados.csv")
-
-# # server.launch(open_browser=False)
+server.launch(open_browser=False)
