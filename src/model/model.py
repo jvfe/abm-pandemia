bf567d71e0b565f3ff819f17089cf72bff8bb58c
JohnVithor <jv.venceslau.c@gmail.com>
2021-04-09 08:37:30 -0300

    Adicionada mais  parametrização, reformulado a forma de criação dos virus, e a mecanica de recuperação/resistencia a eles

--- src/model/model.py
+++ src/model/model.py
@@ -21,15 +21,16 @@ class CovidAgent(Agent):
         initial_state,
         recovery_chance,
         resistance_chance,
-        insert_variant=False,
+        virus=None
     ):
         super().__init__(unique_id, model)
         self.initial_state = initial_state
         self.state = initial_state
         self.recovery_chance = recovery_chance
         self.resistance_chance = resistance_chance
-        self.insert_variant = insert_variant
-        self.virus = insert_variant
+        self.virus = virus
+        self.resistances = []
+        self.time_infected = 0
 
     @property
     def virus(self):
@@ -37,16 +38,27 @@ class CovidAgent(Agent):
 
     @virus.setter
     def virus(self, value):
-        if value == True:
-            self._virus = Variant(self.random.uniform(-1.0, 1.0))
+        # Caso seja uma variante do virus
+        if isinstance(value, Variant) :
+            # Se ele passar no teste de mutação
+            if self.random.random() < value.variation_chance:
+                # É criado uma nova versão do virus com novos valores, baseados no virus original
+                self._virus = Variant(value.spread_chance, value.fatality_rate, value.min_time_to_recover, self.random.uniform(-1.0, 1.0), value.variation_chance)
+            else:
+                # Senão, o virus original que é usado
+                self._virus = value
             self.spread_chance = self._virus.spread_chance
             self.fatality_rate = self._virus.fatality_rate
-        elif value == False:
-            self._virus = Common()
+        # Caso seja um virus comum
+        elif isinstance(value, Common):
+            # Usamos apenas ele mesmo
+            self._virus = value
             self.spread_chance = self._virus.spread_chance
             self.fatality_rate = self._virus.fatality_rate
         else:
             self._virus = None
+            self.spread_chance = 0
+            self.fatality_rate = 0
 
     def try_to_infect(self):
         neighboring_cells = self.model.grid.get_neighbors(
@@ -58,23 +70,31 @@ class CovidAgent(Agent):
         ]
 
         for neighbor in susceptible_neighbors:
-            neighbor.state = State.EXPOSED
-            neighbor.insert_variant = self.insert_variant
-            neighbor.virus = self.insert_variant
+            # Caso o agente não tenha resistencia a esse virus
+            if(self.virus not in neighbor.resistances):
+                # Ele fica exposto ao virus
+                neighbor.state = State.EXPOSED
+                neighbor.virus = self.virus
 
     def try_to_recover(self):
-        if self.random.random() < self.recovery_chance:
-            # Agente se recuperou mas continua suscetível
-            self.state = State.SUSCEPTIBLE
-            # Mas ele consegue se tornar resistente?
-            if self.random.random() < self.resistance_chance:
-                self.state = State.RESISTANT
-        elif self.random.random() < self.fatality_rate:
-            # Se falhou na recuperação, ver se evolui para um caso fatal
-            self.state = State.DEAD
-        else:
-            # Agente continua infectado
-            self.state = State.INFECTED
+        # Adiciona mais uma unidade para o tempo de infecção
+        self.time_infected += 1
+        # Verifica se o agente já pode tentar se recuperar
+        if(self.time_infected >= self.virus.min_time_to_recover):
+            if self.random.random() < self.recovery_chance:
+                # Agente se recuperou mas continua suscetível
+                self.state = State.SUSCEPTIBLE
+                # Mas ele consegue se tornar resistente?
+                if self.random.random() < self.resistance_chance:
+                    # self.state = State.RESISTANT
+                    self.resistances.append(self.virus)
+                    self.virus = None
+            elif self.random.random() < self.fatality_rate:
+                # Se falhou na recuperação, ver se evolui para um caso fatal
+                self.state = State.DEAD
+            else:
+                # Agente continua infectado
+                self.state = State.INFECTED
 
     def check_if_virus_developed(self):
         if self.random.random() < self.spread_chance:
@@ -112,6 +132,10 @@ class CovidModel(Model):
         n_susceptible,
         n_infected,
         insert_variant,
+        spread_chance,
+        fatality_rate,
+        variation_chance,
+        min_time_recovery,
         recovery_chance=0.2,
         resistance_chance=0.01,
         width=50,
@@ -126,7 +150,10 @@ class CovidModel(Model):
         self.recovery_chance = recovery_chance
         self.resistance_chance = resistance_chance
         self.running = True
-
+        if(insert_variant):
+            virus = Variant(spread_chance, fatality_rate, min_time_recovery, self.random.uniform(-1.0, 1.0), variation_chance)
+        else:
+            virus = Common(spread_chance, fatality_rate, min_time_recovery)
         for i in range(self.total_agents):
             if i < self.num_infected:
                 a = CovidAgent(
@@ -135,7 +162,7 @@ class CovidModel(Model):
                     State.INFECTED,
                     self.recovery_chance,
                     self.resistance_chance,
-                    insert_variant,
+                    virus,
                 )
             else:
                 a = CovidAgent(
@@ -145,13 +172,10 @@ class CovidModel(Model):
                     self.recovery_chance,
                     self.resistance_chance,
                 )
-
             self.schedule.add(a)
-
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
-
         self.datacollector = DataCollector(
             model_reporters={
                 "Susceptible": number_susceptible,
