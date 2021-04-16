671a9999a3d15919b7891cf7e06e058cef12febd
jvfe <jvfecav@gmail.com>
2021-04-16 08:27:39 -0300

    refactor: Adiciona parametros do modelo final

--- src/batch_run.py
+++ src/batch_run.py
@@ -4,7 +4,7 @@ from mesa.batchrunner import BatchRunner
 modelo_normal = CovidModel(
     1500,
     3,
-    spread_chance=0.4,
+    spread_chance=0.2,
     fatality_rate=0.024,
     variation_chance=0.01,
     min_time_recovery=1,
@@ -14,10 +14,25 @@ modelo_normal = CovidModel(
     height=60,
     seed=1024,
 )
+
+modelo_normal_lockdown = CovidModel(
+    1500,
+    3,
+    spread_chance=0.2,
+    fatality_rate=0.024,
+    variation_chance=0.01,
+    min_time_recovery=1,
+    move_chance=0.25,
+    insert_variant=False,
+    width=60,
+    height=60,
+    seed=1024,
+)
+
 modelo_variante = CovidModel(
     1500,
     3,
-    spread_chance=0.4,
+    spread_chance=0.2,
     fatality_rate=0.024,
     variation_chance=0.01,
     min_time_recovery=1,
@@ -28,23 +43,46 @@ modelo_variante = CovidModel(
     seed=1024,
 )
 
-for _ in range(2000):
-    modelo_normal.step()
+modelo_variante_lockdown = CovidModel(
+    1500,
+    3,
+    spread_chance=0.2,
+    fatality_rate=0.024,
+    variation_chance=0.01,
+    min_time_recovery=1,
+    move_chance=0.25,
+    insert_variant=True,
+    width=60,
+    height=60,
+    seed=1024,
+)
 
+print("Rodando modelo normal por 500 iterações")
+for _ in range(500):
+    modelo_normal.step()
+    modelo_normal_lockdown.step()
 
 data = modelo_normal.datacollector.get_model_vars_dataframe()
 
-print(f"Rodando modelo normal por 2000 iterações:\n{data}")
+data_lockdown = modelo_normal_lockdown.datacollector.get_model_vars_dataframe()
 
-data.to_csv("../data/modelo_comum_dados.csv")
+data.to_csv("../data/modelo_comum.csv")
+data_lockdown.to_csv("../data/modelo_comum_lockdown.csv")
 
-for _ in range(2000):
+print("Rodando modelo variante por 500 iterações")
+for _ in range(500):
     modelo_variante.step()
+    modelo_variante_lockdown.step()
 
 data_variante = modelo_variante.datacollector.get_model_vars_dataframe()
-print(f"Rodando modelo variante por 2000 iterações:\n{data_variante}")
+data_variante_lockdown = (
+    modelo_variante_lockdown.datacollector.get_model_vars_dataframe()
+)
+
+data_variante.to_csv("../data/modelo_variante.csv")
+data_variante_lockdown.to_csv("../data/modelo_variante_lockdown.csv")
+
 
 data_agentes_variante = modelo_variante.datacollector.get_agent_vars_dataframe()
 
-data_variante.to_csv("../data/modelo_variante_dados.csv")
 data_agentes_variante.to_csv("../data/agentes_variante.csv")
